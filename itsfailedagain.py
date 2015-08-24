08548ff6118b94e4812b41d0d7437d3b26f28301
Martin Barnes <martinpb1l1l1l@gmail.com>
2015-08-24 22:46:56 +0100

    created user and role classes

--- itsfailedagain.py
+++ itsfailedagain.py
@@ -1,12 +1,17 @@
 from datetime import datetime
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, session, flash, redirect
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'this is just a tutorial so dont kill me on git'
@@ -14,15 +19,22 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TAREDOWN'] = True
+db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name')
+        session['name'] = form.name.data
         form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+        redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -41,8 +53,30 @@ def internal_server_error(e):
 
 
 class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # relationship within role table
+    user = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # define foreign key to link the tables together
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r' % self.username
 
 
 if __name__ == '__main__':
