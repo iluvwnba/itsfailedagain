0064dec3b96087ccf5e3df263409b96418fb399e
Martin Barnes <martinpb1l1l1l@gmail.com>
2015-08-25 21:33:37 +0100

    Added migration and index code for views

--- itsfailedagain.py
+++ itsfailedagain.py
@@ -1,15 +1,14 @@
-from datetime import datetime
 import os
 
-from flask import Flask, render_template, url_for, session, flash, redirect
-from flask.ext.script import Manager
+from flask import Flask, render_template, url_for, session, redirect
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask.ext.sqlalchemy import SQLAlchemy
-
+from flask.ext.migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -23,18 +22,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_COMMIT_ON_TAREDOWN'] = True
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<name>')
@@ -52,6 +56,10 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
@@ -62,10 +70,10 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     # relationship within role table
-    user = db.relationship('User', backref='role')
+    user = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
-        return '<Role %r' % self.name
+        return '<Role %r>' % self.name
 
 
 class User(db.Model):
@@ -76,8 +84,10 @@ class User(db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
-        return '<User %r' % self.username
+        return '<User %r>' % self.username
 
 
 if __name__ == '__main__':
+    manager.add_command("shell", Shell(make_context=make_shell_context))
+    manager.add_command('db', MigrateCommand)
     manager.run()
