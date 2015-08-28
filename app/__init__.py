cdb58f0e63faf04a2c298a20dd1b9feced1f2287
Martin Barnes <martinpb1l1l1l@gmail.com>
2015-08-28 23:31:00 +0100

    Total restruct of application

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    from .main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint)
 
     bootstrap.init_app(app)
     mail.init_app(app)
---
+++ app/models.py
@@ -0,0 +1,24 @@
+__author__ = 'Martin'
+from . import db
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # relationship within role table
+    user = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # define foreign key to link the tables together
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
