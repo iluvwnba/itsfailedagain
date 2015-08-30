6be9969b32bbc44281abe996818ee1c2abb48bd6
Martin Barnes <martinpb1l1l1l@gmail.com>
2015-08-30 23:44:05 +0100

    Changed model for email New tests on user model Implemented login/logout

---
+++ app/auth/forms.py
@@ -0,0 +1,12 @@
+__author__ = 'Martin'
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, length
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log in')
+
