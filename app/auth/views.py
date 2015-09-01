f9e377995dfdd2380fd8e86b54b5c1ea46d0c8a0
Martin Barnes <brusebox2000@gmail.com>
2015-09-02 00:46:18 +0100

    Added before request to check for user auth and unconfirm view. Need html

--- app/auth/views.py
+++ app/auth/views.py
@@ -52,5 +52,14 @@ def confirm(token):
         flash('Your confirmation link is invalid or expired')
     return redirect(url_for('main.index'))
 
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+        return redirect(url_for('auth.unconfirmed'))
 
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous() and current_user.confirmed():
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
